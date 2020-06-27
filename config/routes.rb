9125735f45284cce642885dd7da8e18dcabcd623
omar@dostuffmedia.com <om4rezz@MacBook-Pro.ninja.box>
2020-06-28 00:45:25 +0200

    first commit of some scaffolds

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+
+  resources :projects do
+    resources :tasks
+  end
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
+
+  devise_for :users
+  devise_scope :users do
+    authenticated :user do
+      root to: "projects#index"
+    end
+    unauthenticated do
+      root to: "home#index", as: :unauthenticated_root
+    end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
